f5166be741bcc1b155cfda8f3976a2b1abed5e5a
Sparsh Kapoor <sparsh.1923ec1133@kiet.edu>
2022-08-02 01:40:54 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
+import React,{useState} from "react";
 import ExpenseItem from "./components/ExpenseItem";
-import expenses from "./Expenses.jsx";
+import intialExpenses from "./Expenses.jsx";
 import "./components/ExpenseItem.css"
 import NewExpense from "./components/new/NewExpense";
+import ExpensesFilter from "./components/Filter";
 // import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  const addExpense=(expense)=>{
-    console.log("from app.js");
-    console.log(expense);
+  const [expense,setExpense]=useState(intialExpenses);
+  const [year,setYear]=useState("");
+  const filterChangeHandler =selectedYear=>{
+    setYear(selectedYear);
+  }
 
+  const addExpense=(exp)=>{
+    //method 1 direct approach 
+    setExpense([exp, ...expense]);
+    //second one using fucntion prev state type 
+    // setExpense(prevExp=>{
+    //   return([exp,...prevExp]);
+    // })
+
+
+
+  
   }
   return (
     <>
-      <NewExpense onAddExpense={addExpense}/>
+      <NewExpense onAddExpense={addExpense} />
       <div className="expenses">
-        {expenses.map((val) => {
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
+        {expense.filter(exp=>exp.date.getFullYear().toString() === year).map((val) => {
           return (
-            <ExpenseItem key={val.amount}
+            <ExpenseItem
+              key={val.id}
               title={val.title}
               date={val.date}
               amount={val.amount}
